3ef09e1efa4fb81a4eb2fdf1610bb51b5027d8b4
SamhithBharadwaj <samhith.bharadwaj@syncron.com>
2018-05-19 07:21:10 +0530

    Initial setup

---
+++ Banking/src/com/banking/application/BasicUser.java
@@ -0,0 +1,12 @@
+package com.banking.application;
+
+public class BasicUser {
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String middleName;
+	private Integer primaryMobileNumber;
+	private Integer secondaryMobileNumber;
+	private String emailId;
+
+}
---
+++ Banking/src/com/banking/application/User.java
@@ -0,0 +1,7 @@
+package com.banking.application;
+import java.util.calender;
+
+public class User extends BasicUser{
+private Calender calender;
+
+}
